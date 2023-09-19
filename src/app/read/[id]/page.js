e7a69456768c1e5ddcf2e8cf89d2017b292922c8
seojeongwoo <seojeongwoo0902@r2ware.com>
2023-09-19 15:38:01 +0900

    first commit

---
+++ src/app/read/[id]/page.js
@@ -0,0 +1,13 @@
+
+export default async function Read(props) {
+    const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`,{cache:'no-store'})
+    const topics = await resp.json()
+    console.log(topics)
+    return (
+        <>
+            <h2>{topics.title}</h2>
+            {topics.body}
+        </>
+    );
+}
+
